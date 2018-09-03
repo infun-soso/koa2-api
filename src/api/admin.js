30ef5f7c49398aaae2be9a8116813c63b8c4dbd1
infun-soso <973394690@qq.com>
2018-09-04 00:13:00 +0800

    add Interface

--- src/api/admin.js
+++ src/api/admin.js
@@ -2,14 +2,52 @@ const router = require('koa-router')()
 const Article = require('../model/article.js')
 
 router.get('/index', async (ctx, next) => {
-	ctx.response.body = 'this issss admin index'
-	await next()
+	let req = ctx.request.body
+	await  Article.find().then(result => {
+		if (result) {
+			ctx.response.body = {
+				"code": 0,
+				"data": result,
+				"msg": 'success'
+			}
+		} else {
+			ctx.response.body = {
+				"code": 1,
+				"data": [],
+				"msg": 'error'
+			}
+		} 
+	}).catch(err => {
+		console.log(err)
+	})
 })
 
 router.post('/addarticle', async (ctx, next) => {
-	console.log(ctx.request.body)
-	ctx.response.body = `<h1>Welcome,!</h1>`
-	await next()
+	let req = ctx.request.body
+	const newLine = new Article({
+		title: req.articleTitle,
+		keyword: req.keywords,
+		descript: req.description,
+		content: req.content
+	})
+
+	await newLine.save().then(res => {
+		if (res) {
+			ctx.response.body = {
+				"code": 0,
+				"data": [],
+				"msg": 'success'
+			}
+		} else {
+			ctx.body = {
+				code: 1,
+				data: [],
+				msg: 'error'
+			}
+		}
+	}).catch(err => {
+		console.log(err)
+	}) 
 })
 
 module.exports = router
